79dd617863309371b0da405e567cc538d60b5b7e
Dashboy1998 <18587674+Dashboy1998@users.noreply.github.com>
2024-03-16 03:18:59 -0400

    Fixed linting error

--- scripts/undo_move.sh
+++ scripts/undo_move.sh
@@ -3,5 +3,7 @@
 csv_file="compare_srt_renaming_history.csv"
 
 while IFS=',' read -r origial_name predicted_name percentage_match; do
+    # Used to stop linting error
+    echo "$percentage_match" > /dev/null
     mv "$predicted_name" "$origial_name"
 done < "$csv_file"
